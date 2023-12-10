03db90049abb414db6e011454be691e48d73ccc8
Shubham Tiwari <shubht3303@gmail.com>
2023-12-10 16:17:14 +0530

    PP
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

--- DSA-cpp/Patterns/Pattern10.cpp
+++ DSA-cpp/Patterns/Pattern10.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main() {
  int n;
  cin>> n;
+ char ch = 'A' ;
 
  int i = 1;
  while(i<=n) {
   int j=1;
   while (j<=n) {
-   char ch = 'A' + i - 1;
    cout << ch << " ";
    j++;
+   ch++;
   }
   cout<<endl;
   i++;
--- DSA-cpp/Patterns/Pattern9.cpp
+++ DSA-cpp/Patterns/Pattern9.cpp
@@ -9,7 +9,7 @@ int main() {
  while(i<=n) {
   int j=1;
   while (j<=n) {
-   char ch = 'A' + i - 1;
+   char ch = 'A' + j - 1;
    cout << ch << " ";
    j++;
   }
