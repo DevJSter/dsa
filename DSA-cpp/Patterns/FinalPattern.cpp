70ce40da0b61b2b790970a2253db543728fd3eca
Shubham Tiwari <shubht3303@gmail.com>
2023-12-10 19:16:17 +0530

    Pattern18
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

--- DSA-cpp/Patterns/FinalPattern.cpp
+++ DSA-cpp/Patterns/FinalPattern.cpp
@@ -1,1 +1,34 @@
 #include <iostream>
+using namespace std;
+
+
+int main() {
+
+ int n,i=1;
+
+ cin>>n;
+
+ while (i<=n) {
+  int space = n-i;
+  //Print karo spaces ko
+  while (space) {
+   cout<<" ";
+   space--;
+  }
+  //Print karao Pahla traingle
+  int j =1;
+  while (j<=i) {
+   cout << j ;
+   j++;
+  }
+  //Print karo Dusra traingle
+  int k = 1;
+  while (k<=i-1) {
+   cout << i-k;
+   k++;
+  }
+  cout<<endl;
+  i++;
+
+ }
+}
