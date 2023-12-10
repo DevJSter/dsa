7b1f9a024bf7c59a6f7198cc1358d9e531ccbf89
Shubham Tiwari <shubht3303@gmail.com>
2023-12-10 18:35:34 +0530

    Pattern16
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

--- DSA-cpp/Patterns/Pattern15.cpp
+++ DSA-cpp/Patterns/Pattern15.cpp
@@ -7,16 +7,14 @@ int main () {
  int i =1;
 
  while (i<=n) {
-  int j = 1;
-  char ch = 'A' + n + i -2;
+  int j = 1; //For starting character of each row we typecast 'a' and then plus the n and minus i
+  char ch = 'A' + n - i ; //Before logic char ch = 'A' + n -i + 2;
   while (j<=i) {
    cout <<ch << " ";
    j++;
    ch++;
-
   }
   cout<<endl;
   i++;
  }
-
 }
---
+++ DSA-cpp/Patterns/Pattern16.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+ int n; 
+ cin>> n;
+ int i =1;
+
+ while (i<=n) {
+
+  int spaces = n-i;
+  while (spaces) {
+   cout<< " ";
+   spaces--;
+  }
+  int  j =1;
+  while (j<=i) {
+   cout <<j ;
+   j++;
+  }
+  cout<<endl;
+  i++;
+ }
+}
