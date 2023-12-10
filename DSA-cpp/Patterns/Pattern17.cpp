e58411efd2fa7c25ac50abe92acf9e13ba841fd5
Shubham Tiwari <shubht3303@gmail.com>
2023-12-10 18:45:37 +0530

    Pattern17
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

---
+++ DSA-cpp/Patterns/Pattern17.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+ int n,i=1;
+
+ cin>>n;
+
+ while (i<=n) {
+  int j = 1;
+
+  while (j<=n - i +1) {
+   cout<< "*";
+   j++;
+  }
+  cout<<endl;
+  i++;
+ }
+}
+
