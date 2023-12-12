c4a569b6175546151d7331c94bfffa689135d215
Shubham Tiwari <shubht3303@gmail.com>
2023-12-12 14:15:34 +0530

    Operators
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

--- DSA-cpp/Operators/operators.cpp
+++ DSA-cpp/Operators/operators.cpp
@@ -10,18 +10,8 @@ int main () {
 int n;
  cin >> n;
 
- switch (n) {
-  case 1:
-   cout << "1";
-   break;
-  case 2:
-   cout << "2";
-   break;
-  case 3:
-   cout << "3";
-   break;
-  default:
-   cout << "default";
-   break;
+int  sum = 0 ;
+ for (int i=1; i<=n; i++) {
+     sum += i;
  }
 }
