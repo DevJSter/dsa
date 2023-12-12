cdf6e14e5aeef507e158772471864d6a35fdc15b
Shubham Tiwari <shubht3303@gmail.com>
2023-12-12 14:13:49 +0530

    Operators git pm
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

---
+++ DSA-cpp/Operators/operators.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+using namespace std;
+
+int main () {
+ // int a=1, b;
+ // b= a++;
+ // int c = ++a;
+ // cout << b;
+ // cout << c;
+int n;
+ cin >> n;
+
+ switch (n) {
+  case 1:
+   cout << "1";
+   break;
+  case 2:
+   cout << "2";
+   break;
+  case 3:
+   cout << "3";
+   break;
+  default:
+   cout << "default";
+   break;
+ }
+}
