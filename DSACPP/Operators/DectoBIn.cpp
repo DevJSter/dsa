315553757fc3f4e5a528643b687f30ada79327d0
Shubham Tiwari <shubht3303@gmail.com>
2023-12-17 14:16:29 +0530

    Change
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

--- DSACPP/Operators/DectoBIn.cpp
+++ DSACPP/Operators/DectoBIn.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <math.h>
-using namespace std;
+ #include <iostream>
+ #include <math.h>
+ using namespace std;
 
-int main() {
-  
-  int n;
-  
-  cin>>n;
-  
-  int ans = 0,i = 0 ;
-  
-  while (n!=0) {
+ int main() {
    
-   int bit = n & 1;
+   int n;
    
-   ans =  (bit * pow(10,i) ) + ans;
-    // ans = (bit << i) | ans;
+   cin>>n;
+   
+   int ans = 0,i = 0 ;
+   
+   while (n!=0) {
+    
+    int bit = n & 1;
+    
+    ans =  (bit * pow(10,i) ) + ans;
+     // ans = (bit << i) | ans;
 
-   n =  n >> 1;
-   i++;
-  }
-  cout<< " Answer is " << ans <<endl;  
-}
+    n =  n >> 1;
+    i++;
+   }
+   cout<< " Answer is " << ans <<endl;  
+ }
