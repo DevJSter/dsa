89eaf5744bdc3a954bd0f570dd238c3f0ff88763
Shubham Tiwari <shubht3303@gmail.com>
2023-12-18 17:33:45 +0530

    nCr
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

--- DSACPP/SwitchCaseFunctions/Counting.cpp
+++ DSACPP/SwitchCaseFunctions/Counting.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
-
+//Function Signature
 void printCount(int n) {
+ //Function Body
  for (int i = 0 ; i <= n ; i++ )  {
   cout << i << endl ;
  }
 }
 
 int main () {
+ 
  int n;
+ 
  cout << "Enter n: ";
+ 
  cin >> n;
+ 
+ 
+ //Function Call
+ 
  printCount(n);
+ 
+
+ return 0;
 }
