50107ace2aac6ef984ca0dc2899ae99eefb5d41f
Shubham Tiwari <shubht3303@gmail.com>
2023-12-18 16:36:44 +0530

    Calculator
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

---
+++ DSACPP/SwitchCaseFunctions/functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int power (int a, int b) {
+ int res = 1;
+ for (int i = 0; i < b; i++) {
+  res *= a;
+ }
+ return res;
+}  
+int main () {
+
+  int a, b;
+  cin >> a >> b;
+  int ans = power (a, b);
+ cout << ans <<endl;
+
+}
