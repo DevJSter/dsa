663b5f385db4005f1b5c6372b16555fd0f4ff315
Shubham Tiwari <shubht3303@gmail.com>
2023-12-18 15:20:05 +0530

    Switch Cases
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

--- DSACPP/SwitchCase&Functions/switch.cpp
+++ DSACPP/SwitchCase&Functions/switch.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main () {
- int n ; 
+ int n = 2;
+ switch (n) {
+ case 1: cout << "First" <<endl;
+         break;
+ case 2: cout << "Second" <<endl;
+         break;
+
+ default: cout << "Default" <<endl;
+          break;
+ } 
 }
