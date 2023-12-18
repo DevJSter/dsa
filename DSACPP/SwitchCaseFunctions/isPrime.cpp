6eff811b05d3c07e8b1afc8f6233e38c6da28414
Shubham Tiwari <shubht3303@gmail.com>
2023-12-18 17:39:18 +0530

    isPrime.cpp
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

--- DSACPP/SwitchCaseFunctions/isPrime.cpp
+++ DSACPP/SwitchCaseFunctions/isPrime.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 bool isPrime (int n) {
    for (int i =2 ; i <=n ; i++) {
-    if (n%i ==0) {
+    if (n%i == 0) {
      return 0;
+     break;
     }
     else {
      return 1;
