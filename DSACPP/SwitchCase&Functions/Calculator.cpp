4e234335960c8682af7f48e1035de84ab1c7fed5
Shubham Tiwari <shubht3303@gmail.com>
2023-12-18 16:16:45 +0530

    Switch Cases
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

---
+++ DSACPP/SwitchCase&Functions/Calculator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int a , b;
+ char op;
+
+ cout << "Enter the value of a" <<endl;
+ cin >> a;
+
+ cout << "Enter the value of b" <<endl;
+ cin >> b;
+
+ cout << "Enter the operation to be performed" <<endl;
+ cin >> op;
+ switch (op) {
+  case '+' : cout << "Sum of " <<a <<" and " <<b <<" is " <<a+b <<endl;
+             break;
+  case '-' : cout << "Difference of " <<a <<" and " <<b <<" is " <<a-b <<endl;
+             break;
+  case '*' : cout << "Product of " <<a <<" and " <<b <<" is " <<a*b <<endl;
+             break;
+             
+ }
+}
