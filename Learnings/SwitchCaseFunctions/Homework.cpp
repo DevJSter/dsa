56d7faa5120596d652efea73f1df986951ff4542
Shubham Tiwari <shubht3303@gmail.com>
2023-12-19 00:09:40 +0530

    hw.cpp

--- Learnings/SwitchCaseFunctions/Homework.cpp
+++ Learnings/SwitchCaseFunctions/Homework.cpp
@@ -66,13 +66,83 @@ int gp (int a , int r , int n) {
 }
 
 
-int main() {
- int a , r , n ;
- cout << "Enter the first term , common ratio and the nth term" << endl;
- cin >> a >> r >> n ;
+// int main() {
+//  int a , r , n ;
+//  cout << "Enter the first term , common ratio and the nth term" << endl;
+//  cin >> a >> r >> n ;
  
- int ans = gp(a , r , n);
- cout << ans << endl;
+//  int ans = gp(a , r , n);
+//  cout << ans << endl;
+//  return 0;
+// }
+
+
+//Same thing with while loop
+int main() {
+ int amt ;
+ cout << "Enter the Number" <<endl;
+ cin >> amt;
+
+ int notes[] = {2000 , 500 , 200 , 100 , 50 , 20 , 10 , 5 , 2 , 1};
+ int n = sizeof(notes)/sizeof(int);
+
+ int i = 0;
+ while (amt > 0) {
+   int note = notes[i];
+   if (note <= amt) {
+     int count = amt/note;
+     amt = amt - count*note;
+     cout << note << " * " << count << endl;
+   }
+   i++;
+ }
  return 0;
+
+
 }
 
+
+//Same thing with switch case
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int amt;
+    cout << "Enter the Number" << endl;
+    cin >> amt;
+
+    int notes[] = {2000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+    int n = sizeof(notes) / sizeof(int);
+
+    int i = 0;
+    while (amt > 0) {
+        int note = notes[i];
+        if (note <= amt) {
+            int count = amt / note;
+            amt = amt - count * note;
+
+            // Using switch statement to print the result
+            switch (note) {
+                case 2000:
+                case 500:
+                case 200:
+                case 100:
+                case 50:
+                case 20:
+                case 10:
+                case 5:
+                case 2:
+                case 1:
+                    cout << note << " * " << count << endl;
+                    break;
+                default:
+                    // Handle unexpected notes (if any)
+                    break;
+            }
+        }
+        i++;
+    }
+
+    return 0;
+}
