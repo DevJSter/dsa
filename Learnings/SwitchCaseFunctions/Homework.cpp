8dfb3b1cf98793b48046103072141d80930bca7c
Shubham Tiwari <shubht3303@gmail.com>
2023-12-18 23:31:43 +0530

    hw.cpp

--- Learnings/SwitchCaseFunctions/Homework.cpp
+++ Learnings/SwitchCaseFunctions/Homework.cpp
@@ -1,14 +1,35 @@
 #include <iostream> 
 using namespace std;
 
-int update (int a) {
-  int ans =  a * a;
-  return ans ;
-}
+// int update (int a) {
+//   int ans =  a * a;
+//   return ans ;
+// }
 
-int main () {
-  int a = 10;
-  a = update(a);
-  cout << a << endl;
-  return 0;
-}
+// int main () {
+//   int a = 10;
+//   a = update(a);
+//   cout << a << endl;
+//   return 0;
+// }
+
+
+// Fibonacci Number till n
+// int fibo (int n) {
+//   if (n==0 || n==1) {
+//     return n;
+//   }
+//   int ans = fibo(n-1) + fibo(n-2);
+//   return ans;
+// }
+
+// int main() {
+//  int n , a , b ;
+//  cin >> n ;
+ 
+//  int ans = fibo(n);
+//  cout << ans << endl;
+//  return 0;
+// }
+
+
