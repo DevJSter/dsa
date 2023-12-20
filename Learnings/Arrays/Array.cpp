423ebc482af044e78b51927f10032d4cc1c79078
Shubham Tiwari <shubht3303@gmail.com>
2023-12-20 18:45:24 +0530

    Arrays

--- Learnings/Arrays/Array.cpp
+++ Learnings/Arrays/Array.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 int main() {
 
- int a[15];
+ int a[3] = {1,2,3};
+ int b[3] = {4,5,6};
+
+ int c[3];
+
+ cout << "Array a: " << endl;
+ for(int i=0; i<3; i++) {
+     cout << a[i] << endl;
+ }
+
 
  return 0;
 
