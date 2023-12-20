d719939133f4e0ed084234feb98ddaaf1eef782d
Shubham Tiwari <shubht3303@gmail.com>
2023-12-20 18:52:30 +0530

    Arrays

--- Learnings/Arrays/Array.cpp
+++ Learnings/Arrays/Array.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
+ int printArray (int theArray[], int sizeOfArray){
+     for(int x=0; x<sizeOfArray; x++) {
+         cout << theArray[x] << endl;
+     }
+ }; 
 
  int a[3] = {1,2,3};
  int b[3] = {4,5,6};
@@ -12,6 +17,9 @@ int main() {
  for(int i=0; i<3; i++) {
      cout << a[i] << endl;
  }
+
+ cout << "Array b: " << endl;
+ printArray(b, 3);
 
  return 0;
 
