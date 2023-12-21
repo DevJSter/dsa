43aa4aa4a3449b924ff27bbeea470e5b2a45c232
Shubham Tiwari <shubht3303@gmail.com>
2023-12-22 01:17:48 +0530

    Arrays

--- Learnings/Arrays/maxMin.cpp
+++ Learnings/Arrays/maxMin.cpp
@@ -1,43 +1,45 @@
 #include <iostream>
+#include <climits> // Include this header for INT_MAX and INT_MIN
 using namespace std;
 
-int getMin( int arr[] , int size) {
- int min = INT8_MAX;
- for (int i = 0 ; i <= size ; i++) {
-  if (arr[i] < min) {
-   min = arr[i];
-  }
-  return min;
- }
+int getMin(int arr[], int size) {
+    int min = INT_MAX; // Correct initialization for minimum value
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
 }
 
-
-int getMax( int arr[] , int size) {
- int max = INT8_MIN;
- for (int i =0 ; i <= size ; i++) {
-  if (arr[i] > max) {
-   max = arr[i];
-  }
-  return max;
- }
+int getMax(int arr[], int size) {
+    int max = INT_MIN; // Correct initialization for maximum value
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 
 int main() {
+    int size;
+    cin >> size;
 
- int size;
- cin >> size;
+    int arr[100];
 
- int arr[100];
- 
- for (int i=0; i<size; i++) {
-     cin >> arr[i];
- }
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
 
- for (int i = 0; i<size ; i++) {
-  cout << "Entered Numbers are : " << arr[i] << " " ;
- }
+    cout << "Entered Numbers are: ";
 
- cout << "Max: " << getMax(arr, size) ;
- cout << "Min: " << getMin(arr, size) ;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
 
+    cout << "Max: " << getMax(arr, size) << endl;
+    cout << "Min: " << getMin(arr, size) << endl;
+
+    return 0; // Add a return statement at the end of the main function
 }
