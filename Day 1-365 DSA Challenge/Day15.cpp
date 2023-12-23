5636591e9dcdc4ec6dbe7815287b0fe38ae66771
Shubham Tiwari <shubht3303@gmail.com>
2023-12-23 23:11:34 +0530

    Day-15 of 365

--- Day 1-365 DSA Challenge/Day15.cpp
+++ Day 1-365 DSA Challenge/Day15.cpp
@@ -25,3 +25,25 @@
 // 2 <= n <= 105
 // points[i].length == 2
 // 0 <= xi, yi <= 109
+
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        int result = 0;
+
+        vector<int> temp;
+
+        for (int i=0; i < points.size() ; i++ ) {
+            temp.push_back(points[i][0]);
+        }
+
+        sort(temp.begin(),temp.end());
+
+          for (int i=1; i < temp.size() ; i++ ) {
+            result = max(result,temp[i]-temp[i-1]);
+        }
+
+        return result;
+    
+    }
+};
