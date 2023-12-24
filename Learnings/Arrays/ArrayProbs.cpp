e9cc83d2768332f863e94131b61dd5287cb31e7a
Shubham Tiwari <shubht3303@gmail.com>
2023-12-24 17:42:12 +0530

    Corrected swapArrays

--- Learnings/Arrays/ArrayProbs.cpp
+++ Learnings/Arrays/ArrayProbs.cpp
@@ -3,20 +3,31 @@
 #include <iostream>
 using namespace std;
 
+
+void printArray(int arr[] , int size){
+
+for (int i = 0 ; i < size ; i++){
+    cout << arr[i] << " ";
+}
+
+}
+
+void swapAlternate(int arr[], int size) {
+    for(int i =0 ; i < size ; i+=2) {
+        if (i+1 < size) {
+            swap(arr[i], arr[i+1]);
+        }
+
+    }
+}
+
+
 int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int temp;
-
-    for (int i = 0; i < n; i += 2) {
-        temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-    }
-
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-
+    int even[10] = {2,4,6,8,10,12,14,16,18,20};
+    int odd[10] = {1,3,5,7,9,11,13,15,17,19};
+    
+    swapAlternate(even, 10);
+    printArray(even, 10);
+    
     return 0;
 }
