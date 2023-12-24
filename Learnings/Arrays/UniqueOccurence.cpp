97eaec26bc7e18f74e814d43713ac61ba81571ef
Shubham Tiwari <shubht3303@gmail.com>
2023-12-25 00:08:40 +0530

    Unique number of occurences

--- Learnings/Arrays/UniqueOccurence.cpp
+++ Learnings/Arrays/UniqueOccurence.cpp
@@ -23,10 +23,50 @@
 // 1 <= arr.length <= 1000
 // -1000 <= arr[i] <= 1000
 
-
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <iostream>
 using namespace std;
 
 int main(){
- 
+ class Solution {
+public:
+    bool uniqueOccurrences(std::vector<int>& arr) {
+        std::unordered_map<int, int> occurrences;
+        std::unordered_set<int> uniqueCounts;
+
+        // Count occurrences of each value in the array
+        for (int num : arr) {
+            occurrences[num]++;
+        }
+
+        // Check for unique occurrences
+        for (const auto& entry : occurrences) {
+            int count = entry.second;
+            if (uniqueCounts.count(count) > 0) {
+                return false; // Non-unique count found
+            }
+            uniqueCounts.insert(count);
+        }
+
+        return true; // All counts are unique
+    }
+};
+
+#include <iostream>
+
+int main() {
+    Solution solution;
+    std::vector<int> arr1 = {1, 2, 2, 1, 1, 3};
+    std::vector<int> arr2 = {1, 2};
+    std::vector<int> arr3 = {-3, 0, 1, -3, 1, 1, 1, -3, 10, 0};
+
+    std::cout << std::boolalpha << solution.uniqueOccurrences(arr1) << std::endl; // Output: true
+    std::cout << std::boolalpha << solution.uniqueOccurrences(arr2) << std::endl; // Output: false
+    std::cout << std::boolalpha << solution.uniqueOccurrences(arr3) << std::endl; // Output: true
+
+    return 0;
+}
+
 }
