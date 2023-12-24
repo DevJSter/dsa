d8d18eb9c3c51e02e727232802c540b089a58204
Shubham Tiwari <shubht3303@gmail.com>
2023-12-24 23:55:13 +0530

    Sort0&1

---
+++ Learnings/Arrays/sort01.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n) {
+ for (int i = 0; i < n; i++) {
+  cout << arr[i] << " ";
+ }
+ cout << endl;
+}
+
+void sortOneZero(int arr[] , int n) {
+
+ int left = 0;
+ int right = n - 1;
+ while(left < right) {
+  while(arr[left] == 0 && left < right) {
+   left++;
+  }
+  while(arr[right] == 1 && left < right) {
+   right--;
+  }
+  if(left < right) {
+   arr[left] = 0;
+   arr[right] = 1;
+   left++;
+   right--;
+  }
+ }
+
+}
+
+int main() {
+ int arr [8] = {1,0,1,0,1,0,1,0};
+
+ sortOneZero(arr, 8);
+ printArray(arr, 8);
+ return 0;
+}
