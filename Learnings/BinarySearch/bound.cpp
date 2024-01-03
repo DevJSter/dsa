f9f2359d0ffd6bc4da6f79f1f25d7fc58ab31a80
Shubham Tiwari <shubht3303@gmail.com>
2024-01-03 19:38:25 +0530

    Firstdect

--- Learnings/BinarySearch/bound.cpp
+++ Learnings/BinarySearch/bound.cpp
@@ -5,7 +5,7 @@ int firstDect (int arr[] ,int n , int key) {
 
  int s = 0 , e = n - 1;
  int mid = (s + e) / 2;
-int ans = -1;
+ int ans = -1;
  while (s<=e) {
   if (arr[mid] == key) {
    ans =mid;
@@ -18,16 +18,18 @@ int ans = -1;
   else if (arr[mid] > key) { //Right me jao
    s= mid+1;
   }
-  mid = s+ (s-e)/2;
+  mid = s+ (e-s)/2;
 
  }
  
- return -1;
+ return ans ;
 }
 
 
 int main () {
  int even[5] = {1,2,3,3,5};
+
+ cout << "First occurence of 3 is at Index"<< firstDect(even,5,3) << endl;
  
  return 0;
 }
