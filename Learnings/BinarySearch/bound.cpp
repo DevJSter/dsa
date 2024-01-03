b1a1318e3c0542c93ab5a2746572353a1f1d0801
Shubham Tiwari <shubht3303@gmail.com>
2024-01-03 19:31:53 +0530

    bound

--- Learnings/BinarySearch/bound.cpp
+++ Learnings/BinarySearch/bound.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int firstDect () {
- int n;
+int firstDect (int arr[] ,int n , int key) {
+
+ int s = 0 , e = n - 1;
+ int mid = (s + e) / 2;
+
+ while (s<=e) {
+  if (arr[mid] == key) {
+
+  }
+  else if (arr[mid] <= key) {
+   
+  }
+  else if (arr[mid] >= key) {
+   
+  }
+
+ }
  
  return 0;
 }
 
 
 int main () {
-
+ int even[5] = {1,2,3,3,5};
  
  return 0;
 }
