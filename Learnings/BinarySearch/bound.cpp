5005006597c4719dd2a34dc965b5c7ad226c08b1
Shubham Tiwari <shubht3303@gmail.com>
2024-01-03 23:44:01 +0530

    Firstdect

--- Learnings/BinarySearch/bound.cpp
+++ Learnings/BinarySearch/bound.cpp
@@ -1,35 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int firstDect (int arr[] ,int n , int key) {
+int firstDect(int arr[], int n, int key) {
+    int s = 0, e = n - 1;
+    int ans = -1;
+    while (s <= e) {
+        int mid = s + (e - s) / 2; // Move this line inside the loop
+        if (arr[mid] == key) {
+            ans = mid;
+            e = mid - 1;
+        } else if (arr[mid] < key) {
+            e = mid - 1;
+        } else if (arr[mid] > key) {
+            s = mid + 1;
+        }
+    }
 
- int s = 0 , e = n - 1;
- int mid = (s + e) / 2;
- int ans = -1;
- while (s<=e) {
-  if (arr[mid] == key) {
-   ans =mid;
-   e=mid-1;
- 
-  }
-  else if (arr[mid] < key) {//left mein jao
-   e= mid-1; 
-  }
-  else if (arr[mid] > key) { //Right me jao
-   s= mid+1;
-  }
-  mid = s+ (e-s)/2;
-
- }
- 
- return ans ;
+    return ans;
 }
 
+int lastDect(int arr[], int n, int key) {
+    int s = 0, e = n - 1;
+    int ans = -1;
+    while (s <= e) {
+        int mid = s + (e - s) / 2; // Move this line inside the loop
+        if (arr[mid] == key) {
+            ans = mid;
+            s = mid + 1;
+        } else if (arr[mid] < key) {
+            e = mid - 1;
+        } else if (arr[mid] > key) {
+            s = mid + 1;
+        }
+    }
 
-int main () {
- int even[5] = {1,2,3,3,5};
+    return ans;
+}
 
- cout << "First occurence of 3 is at Index"<< firstDect(even,5,3) << endl;
- 
- return 0;
+int main() {
+    int even[5] = {1, 2, 3, 3, 5};
+
+    cout << "First occurrence of 3 is at Index " << firstDect(even, 5, 3) << endl;
+    cout << "Last occurrence of 3 is at Index " << lastDect(even, 5, 3) << endl;
+    return 0;
 }
