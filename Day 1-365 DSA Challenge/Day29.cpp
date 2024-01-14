a99a1507f66b501977c9c3f516aa31c3e61c77e5
Shubham Tiwari <shubht3303@gmail.com>
2024-01-14 23:43:00 +0530

    add : day29.cpp

--- Day 1-365 DSA Challenge/Day29.cpp
+++ Day 1-365 DSA Challenge/Day29.cpp
@@ -26,3 +26,34 @@
 
 // The number of nodes in the tree is in the range [2, 5000].
 // 0 <= Node.val <= 105
+
+
+
+
+
+class Solution {
+public:
+    
+    int findMaxDiff(TreeNode* root, int minV, int maxV) {
+        if(!root)
+            return abs(minV-maxV);
+        
+        minV = min(root->val, minV);
+        maxV = max(root->val, maxV);
+
+        int l = findMaxDiff(root->left,  minV, maxV);
+        int r = findMaxDiff(root->right, minV, maxV);
+
+
+        return max(l, r);
+
+    }
+
+    int maxAncestorDiff(TreeNode* root) {
+        int minV = root->val;
+        int maxV = root->val;
+
+        return findMaxDiff(root, minV, maxV);
+   
+    }
+};
