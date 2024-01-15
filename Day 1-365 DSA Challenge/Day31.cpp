13ed90e619ea60ef445e65524254dbac22abb89a
Shubham Tiwari <shubht3303@gmail.com>
2024-01-16 00:28:54 +0530

    add : day31.cpp/Approach2

--- Day 1-365 DSA Challenge/Day31.cpp
+++ Day 1-365 DSA Challenge/Day31.cpp
@@ -72,3 +72,34 @@ public:
     }
     
 };
+
+//Approach 2
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        int losses[100001];
+        for(int i = 1; i <= 100000; i++){
+            losses[i] = -1;
+        }
+
+        int biggestPlayerNum = 0;
+        for(int i = 0; i < matches.size(); i++){
+            if(losses[matches[i][0]] == -1) losses[matches[i][0]] = 0;
+            if(losses[matches[i][1]] == -1) losses[matches[i][1]] = 0;
+
+            losses[matches[i][1]]++;
+
+            biggestPlayerNum = max(biggestPlayerNum, matches[i][0]);
+            biggestPlayerNum = max(biggestPlayerNum, matches[i][1]);
+        }
+        
+        vector<vector<int>> ans(2);
+        for(int i = 1; i <= biggestPlayerNum; i++){
+            if(losses[i] == 0) ans[0].push_back(i);
+            else if(losses[i] == 1) ans[1].push_back(i);
+        }
+
+        return ans;
+    }
+};
+
