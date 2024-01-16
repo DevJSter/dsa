c3e5d9b1d9a90f21c9d65eeacd7ed302dda44d3e
Shubham Tiwari <shubht3303@gmail.com>
2024-01-17 01:35:56 +0530

    fizes

--- pracrice and algos/hare.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    printf("Hare Krsna Hare Krsna \n Krsna Krsna Hare Hare \n Hare Rama Hare Rama \n Rama Rama Hare Hare"); 
-         main(); //It will call the m,ain function Recursively for infinite amount of time!!
-
-    return 0;
-}
