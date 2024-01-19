64528207874b6c30fd483c3f833f55cc7dd45608
Shubham Tiwari <shubht3303@gmail.com>
2024-01-20 00:12:03 +0530

    add : day33.cpp/Approach3

--- Day 1-365 DSA Challenge/Day33.cpp
+++ Day 1-365 DSA Challenge/Day33.cpp
@@ -24,3 +24,29 @@ n == matrix.length == matrix[i].length
 1 <= n <= 100
 -100 <= matrix[i][j] <= 100
 
+class Solution {
+public:
+    int MFS(vector<vector<int>>& A) {
+        int m = A.size();
+        vector<vector<int>> t(m, vector<int>(m));
+        
+        //initialization
+        for(int col = 0; col<m; col++) {
+            t[0][col] = A[0][col];
+        }
+        
+        for(int row = 1; row < m; row++) {
+            for(int col = 0; col < m; col++) {
+                t[row][col] = A[row][col] + min({t[row-1][col], 
+                                                 t[row-1][max(0, col-1)], 
+                                                 t[row-1][min(m-1, col+1)]});
+            }
+        }
+
+        return *min_element(t[m-1].begin(), t[m-1].end());
+        
+    }
+    int minFallingPathSum(vector<vector<int>>& A) { 
+        return MFS(A);
+    }
+};
