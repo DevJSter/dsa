71f2507936f53540a230c3e5b1fc8cfb9e640831
Shubham Tiwari <shubht3303@gmail.com>
2024-01-20 00:12:16 +0530

    add : day33.cpp/Approach3

--- Day 1-365 DSA Challenge/Day33.cpp
+++ Day 1-365 DSA Challenge/Day33.cpp
@@ -1,28 +1,28 @@
-Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+// Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
 
-A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+// A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
 
  
 
-Example 1:
+// Example 1:
 
 
-Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
-Output: 13
-Explanation: There are two falling paths with a minimum sum as shown.
-Example 2:
+// Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+// Output: 13
+// Explanation: There are two falling paths with a minimum sum as shown.
+// Example 2:
 
 
-Input: matrix = [[-19,57],[-40,-5]]
-Output: -59
-Explanation: The falling path with a minimum sum is shown.
+// Input: matrix = [[-19,57],[-40,-5]]
+// Output: -59
+// Explanation: The falling path with a minimum sum is shown.
  
 
-Constraints:
+// Constraints:
 
-n == matrix.length == matrix[i].length
-1 <= n <= 100
--100 <= matrix[i][j] <= 100
+// n == matrix.length == matrix[i].length
+// 1 <= n <= 100
+// -100 <= matrix[i][j] <= 100
 
 class Solution {
 public:
