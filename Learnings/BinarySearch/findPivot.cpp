175a6eb88e8cbdee19a16157bc02698c3b04d5a5
Shubham Tiwari <shubht3303@gmail.com>
2024-01-31 01:55:41 +0530

    add : findPivot.cpp

--- Learnings/BinarySearch/findPivot.cpp
+++ Learnings/BinarySearch/findPivot.cpp
@@ -1,28 +1,39 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
-int getPivot(int arr[], int n) {
- int s = 0;
- int e = n-1;
+int getPivot(int arr[], int n)
+{
+  int s = 0;
+  int e = n - 1;
 
-
- while (s<e) {
-   int mid = s+(e-s)/2;
-  if (arr[mid] >= arr[0]){
-   s=mid+1;
+  while (s < e)
+  {
+    int mid = s + (e - s) / 2;
+    if (arr[mid] >= arr[0])
+    {
+      s = mid + 1;
+    }
+    else
+    {
+      e = mid;
+    }
   }
-  else{
-   e=mid;
-  }
- }
 
- return arr[s];
-
+  return arr[s];
 }
 
-int main () {
+int main()
+{
 
-int arr[8] = {4,5,6,7,0,1,2,3,};
-cout << "Pivot is " << getPivot(arr, 5) << endl;
-
+  int arr[8] = {
+      4,
+      5,
+      6,
+      7,
+      0,
+      1,
+      2,
+      3,
+  };
+  cout << "Pivot is " << getPivot(arr, 5) << endl;
 }
