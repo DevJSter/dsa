226a97ee8782ada230dbb85a621d18b462095645
Shubham Tiwari <shubht3303@gmail.com>
2024-02-04 23:52:56 +0530

    added : Day 35

--- Day 1-365 DSA Challenge/Da35.cpp
+++ Day 1-365 DSA Challenge/Da35.cpp
@@ -39,3 +39,44 @@
 // 1 <= m, n <= 105
 // s and t consist of uppercase and lowercase English letters.
 
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.length();
+        map<char, int> mp;
+
+        for(char &ch : t) {
+            mp[ch]++;
+        }
+
+        int requiredCount = t.length();
+        int i = 0, j  = 0;
+        int minStart  = 0;
+        int minWindow = INT_MAX;
+        while(j < n) {
+            char ch_j = s[j];
+            if(mp[ch_j] > 0)
+                requiredCount--;
+
+            mp[ch_j]--;
+
+            while(requiredCount == 0) { //try to shrink the window
+                if(minWindow > j-i+1) {
+                    minWindow = j-i+1;
+                    minStart  = i;
+                }
+
+                char ch_i = s[i];
+                mp[ch_i]++;
+                if(mp[ch_i] > 0)
+                    requiredCount++;
+                i++;
+            }
+
+            j++; //Don't ever forget this :-)
+        }
+
+        return minWindow == INT_MAX ? "" : s.substr(minStart, minWindow);
+    }
+};
+
