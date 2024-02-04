43338478a4ea09b1b2a598165c51366a1429ecae
Shubham Tiwari <shubht3303@gmail.com>
2024-02-04 23:52:32 +0530

    added : Day 35

--- Day 1-365 DSA Challenge/Da35.cpp
+++ Day 1-365 DSA Challenge/Da35.cpp
@@ -1,40 +1,41 @@
- 76. Minimum Window Substring
-Hard
+//  76. Minimum Window Substring
+// Hard
 
-17240
+// 17240
 
-701
+// 701
 
-Add to List
+// Add to List
 
-Share
-Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+// Share
+// Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
 
-The testcases will be generated such that the answer is unique.
+// The testcases will be generated such that the answer is unique.
 
  
 
-Example 1:
+// Example 1:
 
-Input: s = "ADOBECODEBANC", t = "ABC"
-Output: "BANC"
-Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
-Example 2:
+// Input: s = "ADOBECODEBANC", t = "ABC"
+// Output: "BANC"
+// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+// Example 2:
 
-Input: s = "a", t = "a"
-Output: "a"
-Explanation: The entire string s is the minimum window.
-Example 3:
+// Input: s = "a", t = "a"
+// Output: "a"
+// Explanation: The entire string s is the minimum window.
+// Example 3:
 
-Input: s = "a", t = "aa"
-Output: ""
-Explanation: Both 'a's from t must be included in the window.
-Since the largest window of s only has one 'a', return empty string.
+// Input: s = "a", t = "aa"
+// Output: ""
+// Explanation: Both 'a's from t must be included in the window.
+// Since the largest window of s only has one 'a', return empty string.
  
 
-Constraints:
+// Constraints:
 
-m == s.length
-n == t.length
-1 <= m, n <= 105
-s and t consist of uppercase and lowercase English letters.
+// m == s.length
+// n == t.length
+// 1 <= m, n <= 105
+// s and t consist of uppercase and lowercase English letters.
+
