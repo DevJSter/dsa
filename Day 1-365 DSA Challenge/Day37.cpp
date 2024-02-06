4ad926a8fd4ac7092fe7e8c834a8d3772db12005
Shubham Tiwari <shubht3303@gmail.com>
2024-02-06 23:15:55 +0530

    day37

--- Day 1-365 DSA Challenge/Day37.cpp
+++ Day 1-365 DSA Challenge/Day37.cpp
@@ -1,37 +1,37 @@
-49. Group Anagrams
-Medium
+// 49. Group Anagrams
+// Medium
 
-18504
+// 18504
 
-564
+// 564
 
-Add to List
+// Add to List
 
-Share
-Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// Share
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
  
 
-Example 1:
+// Example 1:
 
-Input: strs = ["eat","tea","tan","ate","nat","bat"]
-Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-Example 2:
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
 
-Input: strs = [""]
-Output: [[""]]
-Example 3:
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
 
-Input: strs = ["a"]
-Output: [["a"]]
+// Input: strs = ["a"]
+// Output: [["a"]]
  
 
-Constraints:
+// Constraints:
 
-1 <= strs.length <= 104
-0 <= strs[i].length <= 100
-strs[i] consists of lowercase English letters.
+// 1 <= strs.length <= 104
+// 0 <= strs[i].length <= 100
+// strs[i] consists of lowercase English letters.
 
 
