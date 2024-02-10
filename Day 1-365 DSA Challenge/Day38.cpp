b04444a03fe1f538910892a368ac1c3cb9a61716
Shubham Tiwari <shubht3303@gmail.com>
2024-02-10 23:12:45 +0530

    added : day38.cpp

---
+++ Day 1-365 DSA Challenge/Day38.cpp
@@ -0,0 +1,29 @@
+  class Solution {
+  public:
+      int count = 0;
+      void check(string& s, int i, int j, int n) {
+
+          while(i >= 0 && j < n && s[i] == s[j]) {
+              count++;
+              i--; //expanding from center
+              j++; //expanding from center
+          }
+      }
+      int countSubstrings(string s) {
+          int n = s.length();
+          count = 0;
+
+          /*
+
+      Every single character in the string is a center for possible odd-length palindromes: check(s, i, i);
+      Every pair of consecutive characters in the string is a center for possible even-length palindromes: check(s, i, i+1);.
+
+          */
+          for(int i = 0; i<n; i++) {
+              check(s, i, i, n);
+              check(s, i, i+1, n);
+          }
+          return count;
+      }
+  };
+
