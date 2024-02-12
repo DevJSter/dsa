af88ea3ae51f899ca0a98eb98731ca6a3dbe4a9e
Shubham Tiwari <shubht3303@gmail.com>
2024-02-13 02:13:06 +0530

    added : bubbleSort.cpp

--- Learnings/SortingAlgos/bubbleSort.cpp
+++ Learnings/SortingAlgos/bubbleSort.cpp
@@ -1,19 +1,26 @@
 #include <bits/stdc++.h> 
+#include <iostream>
+#include <vector>
+using namespace std;
+
 void bubbleSort(vector<int>& arr, int n)
 {   
-   for (int i=1 ; i<n; i++){
-       
-       for (int j=0 ; j<n-i; j++)
-       if(arr[j] > arr[j+1]) {
-           swap(arr[j],arr[j+1]);
-       }
-   }
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
 }
 
-void main(){
-    vector<int> arr = {5,4,3,2,1};
-    bubbleSort(arr,5);
-    for (int i=0; i<5; i++){
-        cout<<arr[i]<<" ";
+int main() {
+    vector<int> arr = {5, 4, 3, 2, 1};
+    bubbleSort(arr, 5);
+
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
     }
+
+    return 0;
 }
