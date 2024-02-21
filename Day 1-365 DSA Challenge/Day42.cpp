ebef7ede49be8c357387e7c808506e35561a7de5
Shubham Tiwari <shubht3303@gmail.com>
2024-02-21 23:23:53 +0530

    added : Day-42.cpp

--- Day 1-365 DSA Challenge/Day42.cpp
+++ Day 1-365 DSA Challenge/Day42.cpp
@@ -1,31 +1,31 @@
-201. Bitwise AND of Numbers Range
-Medium
+// 201. Bitwise AND of Numbers Range
+// Medium
 
-3781
+// 3781
 
-279
+// 279
 
-Add to List
+// Add to List
 
-Share
-Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+// Share
+// Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
 
  
 
-Example 1:
+// Example 1:
 
-Input: left = 5, right = 7
-Output: 4
-Example 2:
+// Input: left = 5, right = 7
+// Output: 4
+// Example 2:
 
-Input: left = 0, right = 0
-Output: 0
-Example 3:
+// Input: left = 0, right = 0
+// Output: 0
+// Example 3:
 
-Input: left = 1, right = 2147483647
-Output: 0
+// Input: left = 1, right = 2147483647
+// Output: 0
  
 
-Constraints:
+// Constraints:
 
-0 <= left <= right <= 231 - 1
+// 0 <= left <= right <= 231 - 1
