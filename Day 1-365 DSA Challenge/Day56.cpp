048198fe74df7d91f08e306f07116508229fdc97
Shubham Tiwari <shubht3303@gmail.com>
2024-04-29 12:51:44 +0530

    day56.cpp

---
+++ Day 1-365 DSA Challenge/Day56.cpp
@@ -0,0 +1,48 @@
+// 2441 Largest Positive Integer thats exists with its negative
+// Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+
+// Return the positive integer k. If there is no such integer, return -1.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,2,-3,3]
+// Output: 3
+// Explanation: 3 is the only valid k we can find in the array.
+// Example 2:
+
+// Input: nums = [-1,10,6,7,-7,1]
+// Output: 7
+// Explanation: Both 1 and 7 have their corresponding negative values in the array. 7 has a larger value.
+// Example 3:
+
+// Input: nums = [-10,8,6,7,-2,-3]
+// Output: -1
+// Explanation: There is no a single valid k, we return -1.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// -1000 <= nums[i] <= 1000
+// nums[i] != 0
+
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        int result = -1;
+        
+        for (int &i : nums) {
+            for (int &j : nums) {
+                // If there exists a number j such that i is the negative of j
+                if (i == -j) {
+                    // Update the answer to the maximum of current ans and absolute value of i
+                    result = max(result, abs(i));
+                }
+            }
+        }
+
+        return result;
+    }
+};
--- Supreme/patterns/pascaltri.cpp
+++ Supreme/patterns/pascaltri.cpp
@@ -6,11 +6,10 @@ int main(){
     cin >> n;
     int c = 1;
     for (int i = 0; i < n; i++){
-        for (int j = 0; j <= i; j++){
-            cout << c << " ";
-            c++;
-        }
-        cout << endl;
+        cout << c << " ";
+        
+
+        cout << c << " " << endl;
     }
     return 0;
 }
