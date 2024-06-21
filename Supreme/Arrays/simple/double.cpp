cbf5a0a3852b462c450488bce11bd322d38226fb
Shubham Tiwari <shubht3303@gmail.com>
2024-06-21 12:24:52 +0530

    Arrays

--- Supreme/Arrays/simple/double.cpp
+++ Supreme/Arrays/simple/double.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int arr[5] = {1,2,3,4,10};
     for (int i=0; i<5 ; i++){
+        
         cout << "Double of " <<  arr[i] << " is " << arr[i] * 2 << endl;
     }
     return 0;
